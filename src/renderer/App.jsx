8cc0c0e40e44aceb3a72d60f46b2cad87c7de9ab
Danila Abramov <67269761+danilaabramov@users.noreply.github.com>
2023-03-14 16:27:05 +0400

    Add files via upload

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -8,19 +8,22 @@ const Calculator = () => {
   const [weight, setWeight] = useState("");
   const [result, setResult] = useState();
   const [result2, setResult2] = useState();
+  const [result3, setResult3] = useState();
 
   const calculateCost = () => {
     let S = 0;
     let K = 0;
+    let Y = 0;
     if (transportType === "railway") {
       // расчет для ж/д
-      const M = 65;
+      const M = 65000;
       const A = 1000;
       const Spos = 57.6;
       const TS = 5000;
       const N = 1.2;
-      S = (M * A + Spos * distance + TS) * N;
-      K = S * 3 / 65000 * weight;
+      S = (weight / M * A * Spos * distance + TS) * N;
+      K = S * 3;
+	Y = K / weight;
     } else if (transportType === "auto") {
       // расчет для авто
       const Spos = 45 * 0.28;
@@ -29,12 +32,14 @@ const Calculator = () => {
       const ZP = 700;
       const N = 1.2;
       S = ((Spos + Pr) * distance + TS + ZP) * N;
-      K = S * 3 / 20 * weight;
+      K = S * 3;
+	Y = K / weight;
     } else if (transportType === "aviation") {
       // расчет для авиации
     }
     setResult(S.toFixed(2));
     setResult2(K.toFixed(2));
+    setResult3(Y.toFixed(2))
   };
 
   return (
@@ -58,7 +63,8 @@ const Calculator = () => {
       </label>
       <button onClick={calculateCost}>Рассчитать</button>
       {result && <p>Стоимость перевозки: {result} рублей</p>}
-      {result2 && <p>Цена для клиентов: {result2} рублей</p>}
+      {result2 && <p>Цена перевозки для клиентов: {result2} рублей</p>}
+	{result3 && <p>Цена перевозки одного киллограмма для клиентов: {result3} рублей</p>}
     </div>
   );
 };
